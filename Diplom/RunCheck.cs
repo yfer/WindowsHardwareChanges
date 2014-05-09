f1de6c6cf9e15899fcb1fa7f66745b56d120b74c
yfer <Barabanov.mikle@gmail.com>
2014-05-09 11:07:16 +0400

    Changed config format file to JSON
    And added "Translation capabilities"

--- Diplom/RunCheck.cs
+++ Diplom/RunCheck.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Management;
-using LibGit2Sharp;
 using System.IO;
+using LibGit2Sharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace RunCheck
 {
     class RuncCheck
@@ -14,7 +17,7 @@ namespace RunCheck
         {
             Console.WriteLine("Processing started");
             var datadir = "./data";
-           
+            
             if (!Repository.IsValid(datadir)) //if repository not exists we create it
             {
                 Console.WriteLine("Empty git database, creating one");
@@ -23,23 +26,34 @@ namespace RunCheck
             }
 
             using (var rep = new Repository(datadir)) {
-                var machinename = Environment.MachineName;
+                var machinename = Environment.MachineName; //TODO: maybe some more unique identifier?
                 if (rep.Branches[machinename] == null) //going to init commit and create branch for this machine, if not exists
                 {
                     Console.WriteLine("New machine, creating branch for it");
                     rep.Checkout(rep.Branches["master"]);
                     rep.CreateBranch(machinename);
-                }
+                }                
                 rep.Checkout(rep.Branches[machinename]);
-
-                foreach (var Key in from str in File.ReadAllLines("./Parameters.txt") where !str.StartsWith("#") select str)
-                {                
-                    ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from " + Key);
+                foreach (var RegistryKey in JObject.Parse(File.ReadAllText("Parameters.json")))
+                {                    
+                    ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from " + RegistryKey.Key);
                     var data = new List<string>();
-                    foreach(var vv in searcher.Get())
-                        foreach (var vvv in vv.Properties)
-                            data.Add(vvv.Name+" "+vvv.Value);
-                    File.WriteAllLines(datadir + "/" + Key + ".txt", data.ToArray());                
+                    if( RegistryKey.Value["fields"]!=null ) //if fields specified
+                    {
+                        foreach (var v in searcher.Get())
+                            foreach (var p in v.Properties)
+                                foreach (var o in RegistryKey.Value["fields"])
+                                    if ( ((JProperty)o).Name == p.Name )
+                                        data.Add(((JProperty)o).Value + " : " + p.Value);                        
+                    }
+                    else
+                    {
+                        foreach (var v in searcher.Get())
+                            foreach (var p in v.Properties)
+                                data.Add(p.Name + " : " + p.Value);
+                    }
+
+                    File.WriteAllLines(datadir + "/" + RegistryKey.Value["name"] + ".txt", data.ToArray());                
                 }
                 rep.Index.Stage("*");
                 
